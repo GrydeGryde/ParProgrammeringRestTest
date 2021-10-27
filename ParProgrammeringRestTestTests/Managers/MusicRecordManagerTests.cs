9881d189a3b65822e16e8d47fefe69433f31a4cb
Asmus Skafte Böttcher <asmu0543@edu.easj.dk>
2021-10-27 12:32:22 +0200

    Potsman Test

---
+++ ParProgrammeringRestTestTests/Managers/MusicRecordManagerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParProgrammeringRestTest.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParProgrammeringRestTest.Models;
+
+namespace ParProgrammeringRestTest.Managers.Tests
+{
+    [TestClass()]
+    public class MusicRecordManagerTests
+    {
+        private MusicRecordManager _manager = new MusicRecordManager();
+        [TestMethod()]
+        public void GetAllRecordsTest()
+        {
+            Assert.AreEqual(2, _manager.GetAllRecords().Count);
+        }
+
+        //[TestMethod()]
+        //public void GetByTitleTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetByArtistTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetByYearTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
